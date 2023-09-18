8c740932e63ec9d17c55725b785b54bfe592d2f0
Aswin178 <aswikjabc@gmail.com>
2023-09-18 12:58:59 +0530

    commit

---
+++ bin/datatypes.dart
@@ -0,0 +1,19 @@
+void main() {
+  //variables used to store values in the program
+  /// to create a variable
+  /// syntax =>datatypes variable_name=value;
+
+  String name = 'anu';
+  int age = 20;
+  int phone = 098765432;
+  double cgpa = 8.1;
+  String email = 'anu23@gmail.com';
+
+  // string interpolation- combine variables to a predefined string
+  // for single  variable use $variable name
+  // more than one variable use ${variables}
+  print("my name is $name");
+  print("my number is $phone");
+  print("$cgpa is my CGPA in graduation");
+  print("my email is $email");
+}
---
+++ bin/first_sample.dart
@@ -0,0 +1,6 @@
+void main() {
+  print("my name is");
+  print(21);
+  print("+91********");
+  print(8.2);
+}
